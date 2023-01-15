f1f1d256a4e9e6c5929fe6ac37e1afa54ee05c8d
nisnevich <arsnisnevich@gmail.com>
2023-01-15 14:39:27 +0400

    Removed run_tests.py as the tests will be run from startup script with python3 -m unittest discover

--- tests/run_tests.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-# discover all test methods under the "tests" directory
-for test_class in unittest.defaultTestLoader.discover("tests"):
-    for test in test_class:
-        result = unittest.TextTestRunner(verbosity=2).run(test)
