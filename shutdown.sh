265f272138c017fe5704f081f3f3fb6e551560e7
nisnevich <arsnisnevich@gmail.com>
2023-01-30 01:36:58 +0400

    Fixed shutdown script

--- shutdown.sh
+++ shutdown.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Stop the bot
-pm2 stop "eco-lazy-consensus-bot"
+pm2 stop "eco-lazy-consensus-bot-beta"
 
 # Remove the bot from PM2's process list
-pm2 delete "eco-lazy-consensus-bot"
+pm2 delete "eco-lazy-consensus-bot-beta"
 
 # Save process list
 pm2 save
